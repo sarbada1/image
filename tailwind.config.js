79c4978c40369457ed73984fb59acd1c86e35757
sarbada1 <sanjelsarbada12@gmail.com>
2024-06-25 16:36:35 +0545

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,34 +5,44 @@ export default {
     "./resources/**/*.js",
     "./resources/**/*.vue",
     'node_modules/preline/dist/*.js',
-
   ],
-  darkMode:'class',
+  darkMode: 'class',
   theme: {
     extend: {
       fontFamily: {
         'dancing-script': ['Dancing Script', 'cursive'],
         'playfair': ['Playfair', 'serif'],
+        'outfit':["Outfit", "sans-serif"],
       },
-      keyframes:{
+      colors: {
+        pink: {
+          '50': '#ffe4e6',
+          '100': '#fecdd3',
+          '200': '#fda4af',
+          '300': '#fb7185',
+          '400': '#f43f5e',
+          '500': '#e11d48',
+          '600': '#be123c',
+          '700': '#9f1239',
+          '800': '#881337',
+          '900': '#701a3e',
+        },
+        primary: '#e11d48', // Choose a shade from the pink palette
+        secondary: '#fb7185', // Choose a shade from the pink palette
+        background: '#ffe4e6', // Choose a shade from the pink palette
+      },
+      keyframes: {
         "fade-out": {
-          "0%": {
-              opacity: 1
-          },
-          "100%": {
-              opacity: 0
-          },
+          "0%": { opacity: 1 },
+          "100%": { opacity: 0 },
+        },
       },
-      },
-      animation:{
+      animation: {
         fadeout: 'fade-out 1s ease-out 0.75s 1',
-
-      }
+      },
     },
   },
   plugins: [
     require('preline/plugin'),
   ],
-
 }
-
